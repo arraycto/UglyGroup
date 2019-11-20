a530784bd2b3ce3d7c4c1be42a057adf31b60343
lldt <596334236@qq.com>
2019-11-20 22:00:50 +0800

    个人信息

--- src/com/yc/uglygroup/biz/impl/FoodBizlmpl.java
+++ src/com/yc/uglygroup/biz/impl/FoodBizlmpl.java
@@ -1,10 +1,12 @@
 package com.yc.uglygroup.biz.impl;
 
+import java.util.List;
 import java.util.Map;
 
 import com.yc.uglygroup.biz.IFoodBiz;
 import com.yc.uglygroup.dao.IFoodDao;
 import com.yc.uglygroup.dao.impl.FoodDaolmpl;
+import com.yc.uglygroup.entity.Foods;
 
 public class FoodBizlmpl implements IFoodBiz{
 
@@ -12,6 +14,12 @@ public class FoodBizlmpl implements IFoodBiz{
 	public int addfood(Map<String,String> map) {
 		IFoodDao dao = new FoodDaolmpl();
 		return dao.addfood(map);
+	}
+
+	@Override
+	public List<Map<String,String>> findfoods(Integer rid, int page, int rows) {
+		IFoodDao dao = new FoodDaolmpl();
+		return dao.findfoods(rid, page, rows);
 	}
 
 }
